26c567b51564e0b4c7e63613d3fee8ddfd270e12
ryozo-t <180362163+ryozo-t@users.noreply.github.com>
2025-02-16 16:50:43 +0900

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,5 +20,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if (\App::enviroment(['production'])){
+            \URL::forceScheme('https');
+        }
     }
 }
+
